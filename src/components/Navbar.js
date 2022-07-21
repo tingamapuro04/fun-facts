ad78d879bebc2fdcf6ae73c7340d44b234f46b72
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-22 00:09:30 +0300

    Add toggle switch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import logo from '../logo.svg';
-import { Button } from 'react-bootstrap';
+import Switch from "react-switch";
 
 function Navbar() {
   return (
     <>
-      <div>
-        <Button variant="primary">Primary</Button>
-        <Button variant="success">Success</Button>
+      <div className="Nav">
+        
         <div className="Icon">
-          <img src={logo} alt="react icon" />
+          <img src={logo} alt="react icon" className="img" />
           <h2>fun facts</h2>
         </div>
         <div className="toggle">
           <h3>Light</h3>
-          <input type="checkbox" role="switch" />
+          <Switch />
           <h3>dark</h3>
         </div>
       </div>
